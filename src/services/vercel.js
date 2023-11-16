fcf49d08089c387eb3c0a76d67a58ff79fd016df
AngelBlackBlue <angel.blackbkue@hotmail.com>
2023-11-15 23:17:49 -0300

    vercel new

--- src/services/vercel.js
+++ src/services/vercel.js
@@ -1,6 +1,6 @@
 
 
-export const URL_VERCEL = "https://backed-dogs.vercel.app"
+export const URL_VERCEL = "https://back-end-dogs-sigma.vercel.app/"
 //export const URL_VERCEL = "http://localhost:3001"
 
 
